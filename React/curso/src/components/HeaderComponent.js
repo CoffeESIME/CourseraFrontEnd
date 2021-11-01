9c0d32f83794c3419dac700daa874b419e542016
CoffeESIME <54255755+CoffeESIME@users.noreply.github.com>
2021-11-01 17:34:43 -0600

    Uncontrolled forms in the header, the data is now in other variable, not in props

--- React/curso/src/components/HeaderComponent.js
+++ React/curso/src/components/HeaderComponent.js
@@ -7,10 +7,82 @@ import { NavLink } from "react-router-dom";
 import { Nav } from "react-bootstrap";
 import { Col } from "react-bootstrap";
 import { Row } from "react-bootstrap";
+import Form from "react-bootstrap/Form";
+import FormGroup from "react-bootstrap/FormGroup";
+import Button from "react-bootstrap/Button";
+import { Modal } from "react-bootstrap";
 class Header extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isNavOpen: false,
+      isModalOpen: false,
+    };
+    this.toggleModal = this.toggleModal.bind(this);
+    this.handleLogin = this.handleLogin.bind(this);
+  }
+
+  toggleModal() {
+    this.setState({
+      isModalOpen: !this.state.isModalOpen,
+    });
+  }
+  handleLogin(event) {
+
+    
+    event.stopPropagation();
+    alert(
+      "Username: " +
+        this.username.value +
+        " Password: " +
+        this.password.value +
+        " Remember: " +
+        this.remember.checked
+    );
+    
+    this.toggleModal();
+    event.preventDefault();
+
+  }
   render() {
     return (
-      <>
+      <div>
+        <Modal show={this.state.isModalOpen} onHide={this.toggleModal}>
+          <Modal.Header closeButton>Login</Modal.Header>
+          <Modal.Body>
+            <Form noValidate  onSubmit={this.handleLogin}>
+              <FormGroup>
+                <Form.Label htmlFor="username">Username</Form.Label>
+                <Form.Control
+                  type="text"
+                  id="username"
+                  name="username"
+                  ref={(input) => (this.username = input)}
+                />
+              </FormGroup>
+              <FormGroup>
+                <Form.Label htmlFor="password">Password</Form.Label>
+                <Form.Control
+                  type="password"
+                  id="password"
+                  name="password"
+                  ref={(input) => (this.password = input)}
+                />
+              </FormGroup>
+              <FormGroup>
+                <Form.Check
+                  type="checkbox"
+                  name="remember"
+                  label="Remember me"
+                  ref={(input) => (this.remember = input)}
+                />
+              </FormGroup>
+              <Button type="submit" value="submit" color="primary">
+                Login
+              </Button>
+            </Form>
+          </Modal.Body>
+        </Modal>
         <Navbar collapseOnSelect expand="lg" variant="dark">
           <Container>
             <NavbarBrand href="/">
@@ -25,14 +97,27 @@ class Header extends Component {
             </NavbarBrand>
             <NavbarToggle aria-controls="responsive-navbar-nav" />
             <Navbar.Collapse id="responsive-navbar-nav">
-
-            <Nav className="me-auto">
-              <NavLink className="nav-link" to="/home"><i className="fa fa-home fa-lg">Home</i></NavLink>
-              <NavLink className="nav-link" to="/aboutus"><i className="fa fa-info fa-lg">About Us</i></NavLink>
-              <NavLink className="nav-link" to="/menu"><i className="fa fa-list fa-lg">Menu</i></NavLink>
-              <NavLink className="nav-link" to="/contactus"><i className="fa fa-address-card fa-lg">Contact</i></NavLink>
-
-            </Nav>
+              <Nav className="me-auto">
+                <NavLink className="nav-link" to="/home">
+                  <i className="fa fa-home fa-lg">Home</i>
+                </NavLink>
+                <NavLink className="nav-link" to="/aboutus">
+                  <i className="fa fa-info fa-lg">About Us</i>
+                </NavLink>
+                <NavLink className="nav-link" to="/menu">
+                  <i className="fa fa-list fa-lg">Menu</i>
+                </NavLink>
+                <NavLink className="nav-link" to="/contactus">
+                  <i className="fa fa-address-card fa-lg">Contact</i>
+                </NavLink>
+              </Nav>
+              <Nav className="ml-auto" navbar>
+                <Nav.Item>
+                  <Button onClick={this.toggleModal}>
+                    <span className="fa fa-sign-in fa-lg"></span> Login
+                  </Button>
+                </Nav.Item>
+              </Nav>
             </Navbar.Collapse>
           </Container>
         </Navbar>
@@ -50,7 +135,7 @@ class Header extends Component {
             </Row>
           </Container>
         </div>
-      </>
+      </div>
     );
   }
 }
